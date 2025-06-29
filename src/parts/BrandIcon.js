0574da82529e83994abf58da4d1bcafe3de6e093
bijaylimbu <bijaylimbu11111@gmail.com>
2025-06-29 21:01:07 +0545

    removing racs

--- src/parts/BrandIcon.js
+++ src/parts/BrandIcon.js
@@ -14,8 +14,8 @@ export default function BrandIcon() {
       href="/"
     >
       <p className="text-theme-blue text-4xl font-medium ">
-        Racx
-        <span className="text-theme-purple">studio</span>
+        Company
+        <span className="text-theme-purple">logo</span>
       </p>
     </Button>
   );
